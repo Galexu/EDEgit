8c0015e725c93aee642088ba59cfd78b5e91c11a
Galex <galexu@gmail.com>
2023-10-23 17:00:18 +0200

    V3_Añadido comparacion de menor

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,11 @@ public class Main {
         } else {
             System.out.println("El numero mayor es: " + num2);
         }
+
+        if (num1 < num2) {
+            System.out.println("El numero menor es: " + num1);
+        } else {
+            System.out.println("El numero menor es: " + num2);
+        }
     }
 }
