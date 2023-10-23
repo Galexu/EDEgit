e695705a89a53d2477a42d4407996cd19e0cd2a0
Galex <galexu@gmail.com>
2023-10-23 16:49:11 +0200

    V2_Implementado algoritmo de obtencion de numero mayor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("dime dos numeros");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
+        if (num1 > num2) {
+            System.out.println("El numero mayor es: " + num1);
+        } else {
+            System.out.println("El numero mayor es: " + num2);
+        }
     }
 }
